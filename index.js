12d93c019b510e45d3e87f95bc303cdb924c3709
Rishabh Rawat <rawatrishabh994@gmail.com>
2025-11-28 01:27:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+    // res.end(req.url);
+
+    if(req.url == "/") {
+        res.writeHead(200);
+        res.end("Home Page");
+    }
+    else if (req.url == "/about") {
+        res.writeHead(200);
+        res.end("About Page");
+    }
+    else if (req.url == "/api") {
+        res.writeHead(200);
+        res.end(JSON.stringify({
+            marvel : "Iron Man",
+            dc : "Superman",
+        }));
+    }
+    else {
+        res.writeHead(404);
+        res.end("Page Not Found");
+    }
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
